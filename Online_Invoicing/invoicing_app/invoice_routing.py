a1b2be2a195e54d312571dce91816ebe273656b2
Keith Cortes <11corteschristopher@gmail.com>
2026-03-23 22:38:19 +0800

    Adding Routing File

--- Online_Invoicing/invoicing_app/invoice_routing.py
+++ Online_Invoicing/invoicing_app/invoice_routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/negotiation/(?P<room_hash>\w+)/$', consumers.NegotiationConsumer.as_asgi()),
+    re_path(r'ws/room/(?P<room_hash>[-\w]+)/$', consumers.NegotiationConsumer.as_asgi()),
 ]
